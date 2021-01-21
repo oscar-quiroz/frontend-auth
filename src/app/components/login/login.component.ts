549a23175ac6978e9b99d539bbf82295dce0fd42
oscar-quiroz <kairox.rt@gmail.com>
2021-01-21 10:18:31 -0500

    ruteo simple generado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from '@angular/router'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router : Router ) { }
 
   ngOnInit(): void {
   }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from '@angular/router'
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router : Router ) { }
 
   ngOnInit(): void {
   }
