e5ab2ef074e4d482ebb418e31fdcf778d833e409
oscar-quiroz <kairox.rt@gmail.com>
2021-01-23 17:15:34 -0500

    actualizando ruteo y pagina de error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//components
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+  path: '',
+  redirectTo: '/login',
+  pathMatch: 'full'
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'register',
+  component: RegisterComponent
+},{
+  path: "**",
+  component: ErrorComponent
+
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
